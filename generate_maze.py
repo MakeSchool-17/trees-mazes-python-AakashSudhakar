9f7ae9b89fdb7d637086a19c7bc16f25b09dd488
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-05-02 16:50:10 -0700

    Just wrote a dequeueueueueueueue

--- generate_maze.py
+++ generate_maze.py
@@ -2,6 +2,30 @@ import maze
 from random import choice, randint
 
 # Create maze using Pre-Order DFS maze creation algorithm
+
+# ======================================= PSEUDOCODE =======================================
+"""
+create a stack for backtracking
+choose a cell index at random from the grid to be current cell
+set visited cells to 1
+
+while visited cells < total cells
+    get unvisited neighbors using cell_neighbors
+    if at least one neighbor
+        choose random neighbor to be new cell
+        knock down wall between it and current cell using connect_cells
+        push current cell to stack
+        set current cell to new cell
+        add 1 to visited cells
+    else
+        pop from stack to current cell
+    call refresh_maze_view to update visualization
+
+set state to 'solve'
+"""
+# /====================================== PSEUDOCODE =======================================
+
+
 def create_dfs(m):
     """ Maze creation algorithm utilizing pre-order depth-first tree traversal. """
     # curr = choice(m.total_cells)            # Chooses cell index at random from grid to be current cell
--- solve_maze.py
+++ solve_maze.py
@@ -4,15 +4,42 @@ import sys
 
 
 # Solve maze using Pre-Order DFS algorithm, terminate with solution
+
 def solve_dfs(m):
     # TODO: Implement solve_dfs
     pass
 
 
 # Solve maze using BFS algorithm, terminate with solution
+# ======================================= PSEUDOCODE =======================================
+"""
+create a queue
+set current cell to 0
+set in direction to 0b0000
+set visited cells to 0
+enqueue (current cell, in direction)
+
+while current cell not goal and queue not empty
+    dequeue to current cell, in direction
+    visit current cell with bfs_visit_cell
+    add 1 to visited cells
+    call refresh_maze_view to update visualization
+
+    get unvisited neighbors of current cell using cell_neighbors, add to queue
+
+trace solution path and update cells with solution data using reconstruct_solution
+
+set state to 'idle'
+"""
+# /====================================== PSEUDOCODE =======================================
 def solve_bfs(m):
-    # TODO: Implement solve_bfs
-    pass
+    queue, curr, in_direction, visited = list(), 0, "0b0000", 0
+    queue.append((curr, in_direction))          # Enqueues current cell and direction to queue
+
+    while (curr != "goal") and len(queue) > 0:
+        queue.pop(0)                            # Dequeuueueueueueues first index
+
+    m.state = "idle"                            # Sets state to idle
 
 
 def print_solution_array(m):
